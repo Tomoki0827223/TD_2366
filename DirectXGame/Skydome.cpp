225574e3973e01f4cd6fd0e6631df1b3f24c8011
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2025-01-22 16:58:56 +0900

    Watada

--- DirectXGame/Skydome.cpp
+++ DirectXGame/Skydome.cpp
@@ -1,9 +1,11 @@
 #include "Skydome.h"
 
 void Skydome::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera) {
+	
 	worldtransfrom_.Initialize();
 	model_ = model;
 	camera_ = camera;
+
 }
 
 void Skydome::Update() {}
